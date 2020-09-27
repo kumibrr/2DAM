2b62a0d8c192a2e49c2e010a89abd15759a4c249
kumibrr <isra.lora99@gmail.com>
2020-09-27 16:52:56 +0200

    Terminada prueba inicial de Libre Configuracion.

--- Libre configuración/001_prueba-inicial/pages/bookings/bookings.js
+++ Libre configuración/001_prueba-inicial/pages/bookings/bookings.js
@@ -1,29 +1,84 @@
 const myURL = '/assets/bookings.xml';
 
-class book {
-    book(place, bookingDate, bookedDate, room, price) {
+class Book {
+    constructor(place, bookingDate, bookedDate, room, price) {
         this.place = place;
-        this.bookingDate = bookingDate;
-        this.bookedDate = bookedDate;
+        this.bookingDate = new Date(bookingDate);
+        this.bookedDate = new Date(bookedDate);
         this.room = room;
         this.price = price;
     }
 }
 
-function getDataFromXML() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+const bookArr = new Array();
+
+function getDataFromXML(url) {
+    return new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+
         if (this.readyState === 4 && this.status === 200) {
-            console.log(this.response);
+            resolve(xhttp.responseXML);
         }
     }
-    xhttp.open("GET", myURL);
+    xhttp.open("GET", url);
     xhttp.send();
+    })
+    
 }
 
+function representData() {
+    
+    tableContainer = document.querySelector('.content');
+    const headerElements = ['Lugar', 'Fecha', 'Fecha de compra', 'Habitacion', 'Precio'];
+    const table = document.createElement('table');
+    const header = document.createElement('tr');
+    headerElements.forEach(element => {
+        const thr = document.createElement('th');
+        thr.textContent = element;
+        header.appendChild(thr);
+    });
+    table.appendChild(header);
+
+    bookArr.forEach(element => {
+        tRow = document.createElement('tr');
+        tdPlace = document.createElement('td');
+        tdPlace.textContent = element.place;
+        tRow.appendChild(tdPlace);
+        tdBookingDate = document.createElement('td');
+        tdBookingDate.textContent = element.bookingDate;
+        tRow.appendChild(tdBookingDate);
+        tdBookedDate = document.createElement('td');
+        tdBookedDate.textContent = element.bookedDate;
+        tRow.appendChild(tdBookedDate);
+        tdRoom = document.createElement('td');
+        tdRoom.textContent = element.room;
+        tRow.appendChild(tdRoom);
+        tdPrice = document.createElement('td');
+        tdPrice.textContent = element.price;
+        tRow.appendChild(tdPrice);
+        table.appendChild(tRow);
+    });
+    tableContainer.appendChild(table);
+}
 
 function main() {
-    getDataFromXML();
+    getDataFromXML(myURL)
+    .then(r => {
+        bookings = r.getElementsByTagName('booking');
+        for (let i = 0; i < bookings.length; i++) {
+            const element = bookings[i];
+            place = element.getElementsByTagName('place')[0].textContent;
+            bookingDate = element.getElementsByTagName('booking_date')[0].textContent;
+            bookedDate = element.getElementsByTagName('booked_date')[0].textContent;
+            room = element.getElementsByTagName('room')[0].textContent;
+            price = element.getElementsByTagName('price')[0].textContent;
+
+            const booking = new Book(place, bookingDate, bookedDate, room, price);
+            bookArr.push(booking);
+        }
+        representData();
+    });
 }
 
 window.addEventListener('load', main);
