eb7f462d5a048e11988049f35cc329db7e1d54f6
Israel de la Barrera <israeldlb@DESKTOP-I6OHN80.localdomain>
2020-10-01 22:56:55 +0200

    Hecho ejercicio de factorial

---
+++ Desarrollo de Interfaces/002_Ejercicio-factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _002_Ejercicio_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el número:");
+            int fact = Int32.Parse(Console.ReadLine());
+
+            int r = 1;
+            for (int i = 1; i - 1 < fact; i++)
+            {
+                r = r * i;
+            }
+            
+            Console.WriteLine($"El factorial de {fact} es: {r}");
+        }
+    }
+}
