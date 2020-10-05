7c80887c73a343428f4d923fa62a4d180883a015
kumibrr <isra.lora99@gmail.com>
2020-10-05 14:38:59 +0200

    Clase Desarrollo de interfaces

---
+++ Desarrollo de Interfaces/003_Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _003_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();    
+        }
+
+        static void ejercicio1() {
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++) {
+                
+                Console.WriteLine($"Introduce el valor {i}: ");
+                try {
+                    arr[i] = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Eso q es herman0 q me lías");
+                    i--;
+                }
+            }
+
+            int greatest = arr[0];
+            int smallest = arr[0];
+            foreach (int n in arr) {
+                if (n > greatest) {
+                    greatest = n;
+                }
+                if (n < smallest) {
+                    smallest = n;
+                }
+            }
+            Console.WriteLine($"El valor mínimo es \" {smallest} \" y el máximo es \' {greatest} \'");
+        }
+    }
+}
