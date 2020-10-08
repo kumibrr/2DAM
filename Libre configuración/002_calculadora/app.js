e7c16a902bb6bd8cf73e69a99015f114da33d59d
kumibrr <isra.lora99@gmail.com>
2020-10-08 14:34:03 +0200

    Añadida algo de lógica a ejercicio de calculadora.

--- Libre configuración/002_calculadora/app.js
+++ Libre configuración/002_calculadora/app.js
@@ -1,13 +1,44 @@
+/*
+    C -> CE => clear
+    P -> % => percentage
+    A -> + => add
+    M -> - => minus
+    D -> / => division
+    T -> x => multiplication
+    E -> = => equals
+    f -> . => floating point
+*/
+
+let input = ''; //keys pressed before doing an operation
+let ac = 0; //acumulator
+const ESP = ['C', 'P', 'A', 'M', 'D', 'T', 'E'];
 
 function register(ev) {
-    console.log(ev.target.value);
+    if (ev.target.value == 'C') {
+        input = '';
+        ac = 0;
+    }
+
+    espCharCounter = 0;
+    
+    input += ev.target.value;
+    
+    ESP.forEach((char) => {
+        if (input.indexOf(char) != -1) {
+            espCharCounter++;
+        }
+    });
+    if (espCharCounter > 1){
+        console.log('operation')
+    }
 }
 
 
 function main() {
-    document.querySelectorAll('button').forEach((item) => {
-        item.addEventListener('click', (ev) => {register(ev)});
+    document.querySelector('.calc-buttons').addEventListener('click', (ev) => {
+        register(ev);
     });
+    ac = document.querySelector('acumulator');
 }
 
 window.addEventListener('load', main);
