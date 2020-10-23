41a870714a50adbd9a906ee8d3a63e78463ca044
Israel <isra.lora99@gmail.com>
2020-10-23 08:56:56 +0200

    DDI: Terminado ejercicio atrasado.

--- Desarrollo de Interfaces/003_Arrays/Program.cs
+++ Desarrollo de Interfaces/003_Arrays/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _003_Arrays
 {
+    struct priceAndDiscount
+    {
+        private double price;
+        private double discount;
+
+        public priceAndDiscount(double price, double discount)
+        {
+            this.price = price;
+            this.discount = discount;
+        }
+
+        public double calcTotal()
+        {
+            return price - (price * discount / 100);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            ejercicio1();    
+            // ejercicio1();   
+            Ejercicio2();
         }
-
+        
         static void ejercicio1() {
             int[] arr = new int[5];
             for (int i = 0; i < arr.Length; i++) {
@@ -22,7 +41,7 @@ namespace _003_Arrays
                     i--;
                 }
             }
-
+        
             int greatest = arr[0];
             int smallest = arr[0];
             foreach (int n in arr) {
@@ -35,5 +54,21 @@ namespace _003_Arrays
             }
             Console.WriteLine($"El valor mínimo es \" {smallest} \" y el máximo es \' {greatest} \'");
         }
+
+        static void Ejercicio2()
+        {
+            string[] items = new[] {"Camisa caballero", "Camisa señora", "Pantalón Caballero", "Pantalón señora"};
+            priceAndDiscount[] pAd = new[]
+            {
+                new priceAndDiscount(25, 10), new priceAndDiscount(18, 5),
+                new priceAndDiscount(33, 2), new priceAndDiscount(35, 0)
+            };
+
+            for (int i = 0; i < items.Length && i < pAd.Length; i++)
+            {
+                Console.WriteLine($"{items[i]} {pAd[i].calcTotal()}€");
+            }
+        }
     }
+        
 }
