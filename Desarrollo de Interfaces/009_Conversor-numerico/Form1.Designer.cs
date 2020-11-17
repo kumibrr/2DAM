35869efc7ff89248eec40eb595bed42c8969064d
kumibrr <isra.lora99@gmail.com>
2020-11-17 13:07:53 +0100

    DI: Terminado ejercicio conversor numérico

--- Desarrollo de Interfaces/009_Conversor-numerico/Form1.Designer.cs
+++ Desarrollo de Interfaces/009_Conversor-numerico/Form1.Designer.cs
@@ -41,6 +41,10 @@
             this.btnConvert = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.imgLebron = new System.Windows.Forms.PictureBox();
+            this.rbtnDecimal = new System.Windows.Forms.RadioButton();
+            this.rbtnHex = new System.Windows.Forms.RadioButton();
+            this.rbtnOctal = new System.Windows.Forms.RadioButton();
+            this.rbtnBin = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.imgLebron)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,8 +54,9 @@
             this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 34F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(510, 70);
+            this.lblTitle.Size = new System.Drawing.Size(765, 108);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Conversor Numérico";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -60,9 +65,10 @@
             // 
             this.lblDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDecimal.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblDecimal.Location = new System.Drawing.Point(42, 98);
+            this.lblDecimal.Location = new System.Drawing.Point(63, 147);
+            this.lblDecimal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDecimal.Name = "lblDecimal";
-            this.lblDecimal.Size = new System.Drawing.Size(140, 23);
+            this.lblDecimal.Size = new System.Drawing.Size(210, 35);
             this.lblDecimal.TabIndex = 1;
             this.lblDecimal.Text = "Número Decimal";
             this.lblDecimal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -71,9 +77,10 @@
             // 
             this.lblBinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBinary.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblBinary.Location = new System.Drawing.Point(42, 134);
+            this.lblBinary.Location = new System.Drawing.Point(63, 202);
+            this.lblBinary.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblBinary.Name = "lblBinary";
-            this.lblBinary.Size = new System.Drawing.Size(140, 23);
+            this.lblBinary.Size = new System.Drawing.Size(210, 35);
             this.lblBinary.TabIndex = 2;
             this.lblBinary.Text = "Binario";
             this.lblBinary.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -82,9 +89,10 @@
             // 
             this.lblOctal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblOctal.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblOctal.Location = new System.Drawing.Point(42, 169);
+            this.lblOctal.Location = new System.Drawing.Point(63, 256);
+            this.lblOctal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOctal.Name = "lblOctal";
-            this.lblOctal.Size = new System.Drawing.Size(140, 23);
+            this.lblOctal.Size = new System.Drawing.Size(210, 35);
             this.lblOctal.TabIndex = 3;
             this.lblOctal.Text = "Octal";
             this.lblOctal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -93,42 +101,47 @@
             // 
             this.lblHex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHex.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblHex.Location = new System.Drawing.Point(42, 204);
+            this.lblHex.Location = new System.Drawing.Point(63, 310);
+            this.lblHex.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHex.Name = "lblHex";
-            this.lblHex.Size = new System.Drawing.Size(140, 23);
+            this.lblHex.Size = new System.Drawing.Size(210, 35);
             this.lblHex.TabIndex = 4;
             this.lblHex.Text = "Hexadecimal";
             this.lblHex.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtDecimal
             // 
-            this.txtDecimal.Location = new System.Drawing.Point(207, 98);
+            this.txtDecimal.Location = new System.Drawing.Point(278, 151);
+            this.txtDecimal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtDecimal.Name = "txtDecimal";
-            this.txtDecimal.Size = new System.Drawing.Size(100, 20);
+            this.txtDecimal.Size = new System.Drawing.Size(148, 26);
             this.txtDecimal.TabIndex = 5;
             // 
             // txtBinary
             // 
             this.txtBinary.Enabled = false;
-            this.txtBinary.Location = new System.Drawing.Point(207, 134);
+            this.txtBinary.Location = new System.Drawing.Point(278, 206);
+            this.txtBinary.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtBinary.Name = "txtBinary";
-            this.txtBinary.Size = new System.Drawing.Size(100, 20);
+            this.txtBinary.Size = new System.Drawing.Size(148, 26);
             this.txtBinary.TabIndex = 6;
             // 
             // txtOctal
             // 
             this.txtOctal.Enabled = false;
-            this.txtOctal.Location = new System.Drawing.Point(207, 169);
+            this.txtOctal.Location = new System.Drawing.Point(278, 260);
+            this.txtOctal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtOctal.Name = "txtOctal";
-            this.txtOctal.Size = new System.Drawing.Size(100, 20);
+            this.txtOctal.Size = new System.Drawing.Size(148, 26);
             this.txtOctal.TabIndex = 7;
             // 
             // txtHex
             // 
             this.txtHex.Enabled = false;
-            this.txtHex.Location = new System.Drawing.Point(207, 204);
+            this.txtHex.Location = new System.Drawing.Point(278, 314);
+            this.txtHex.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtHex.Name = "txtHex";
-            this.txtHex.Size = new System.Drawing.Size(100, 20);
+            this.txtHex.Size = new System.Drawing.Size(148, 26);
             this.txtHex.TabIndex = 8;
             // 
             // btnConvert
@@ -138,9 +151,10 @@
             this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConvert.ForeColor = System.Drawing.SystemColors.Window;
-            this.btnConvert.Location = new System.Drawing.Point(46, 267);
+            this.btnConvert.Location = new System.Drawing.Point(69, 411);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(261, 23);
+            this.btnConvert.Size = new System.Drawing.Size(392, 35);
             this.btnConvert.TabIndex = 9;
             this.btnConvert.Text = "CONVERTIR";
             this.btnConvert.UseVisualStyleBackColor = false;
@@ -152,9 +166,10 @@
             this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.DarkGreen;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.ForeColor = System.Drawing.Color.White;
-            this.btnExit.Location = new System.Drawing.Point(46, 296);
+            this.btnExit.Location = new System.Drawing.Point(69, 455);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(261, 23);
+            this.btnExit.Size = new System.Drawing.Size(392, 35);
             this.btnExit.TabIndex = 10;
             this.btnExit.Text = "SALIR";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -163,18 +178,65 @@
             // imgLebron
             // 
             this.imgLebron.Image = ((System.Drawing.Image)(resources.GetObject("imgLebron.Image")));
-            this.imgLebron.Location = new System.Drawing.Point(349, 98);
+            this.imgLebron.Location = new System.Drawing.Point(524, 151);
+            this.imgLebron.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.imgLebron.Name = "imgLebron";
-            this.imgLebron.Size = new System.Drawing.Size(149, 221);
+            this.imgLebron.Size = new System.Drawing.Size(224, 340);
             this.imgLebron.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgLebron.TabIndex = 11;
             this.imgLebron.TabStop = false;
             // 
+            // rbtnDecimal
+            // 
+            this.rbtnDecimal.AutoSize = true;
+            this.rbtnDecimal.Checked = true;
+            this.rbtnDecimal.Location = new System.Drawing.Point(440, 154);
+            this.rbtnDecimal.Name = "rbtnDecimal";
+            this.rbtnDecimal.Size = new System.Drawing.Size(21, 20);
+            this.rbtnDecimal.TabIndex = 12;
+            this.rbtnDecimal.TabStop = true;
+            this.rbtnDecimal.UseVisualStyleBackColor = true;
+            this.rbtnDecimal.CheckedChanged += new System.EventHandler(this.rbtnDecimal_CheckedChanged);
+            // 
+            // rbtnHex
+            // 
+            this.rbtnHex.AutoSize = true;
+            this.rbtnHex.Location = new System.Drawing.Point(440, 317);
+            this.rbtnHex.Name = "rbtnHex";
+            this.rbtnHex.Size = new System.Drawing.Size(21, 20);
+            this.rbtnHex.TabIndex = 13;
+            this.rbtnHex.UseVisualStyleBackColor = true;
+            this.rbtnHex.CheckedChanged += new System.EventHandler(this.rbtnHex_CheckedChanged);
+            // 
+            // rbtnOctal
+            // 
+            this.rbtnOctal.AutoSize = true;
+            this.rbtnOctal.Location = new System.Drawing.Point(440, 263);
+            this.rbtnOctal.Name = "rbtnOctal";
+            this.rbtnOctal.Size = new System.Drawing.Size(21, 20);
+            this.rbtnOctal.TabIndex = 14;
+            this.rbtnOctal.UseVisualStyleBackColor = true;
+            this.rbtnOctal.CheckedChanged += new System.EventHandler(this.rbtnOctal_CheckedChanged);
+            // 
+            // rbtnBin
+            // 
+            this.rbtnBin.AutoSize = true;
+            this.rbtnBin.Location = new System.Drawing.Point(440, 209);
+            this.rbtnBin.Name = "rbtnBin";
+            this.rbtnBin.Size = new System.Drawing.Size(21, 20);
+            this.rbtnBin.TabIndex = 15;
+            this.rbtnBin.UseVisualStyleBackColor = true;
+            this.rbtnBin.CheckedChanged += new System.EventHandler(this.rbtnBin_CheckedChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 347);
+            this.ClientSize = new System.Drawing.Size(765, 534);
+            this.Controls.Add(this.rbtnBin);
+            this.Controls.Add(this.rbtnOctal);
+            this.Controls.Add(this.rbtnHex);
+            this.Controls.Add(this.rbtnDecimal);
             this.Controls.Add(this.imgLebron);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnConvert);
@@ -188,6 +250,7 @@
             this.Controls.Add(this.lblDecimal);
             this.Controls.Add(this.lblTitle);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Conversor Numérico";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -211,6 +274,10 @@
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.PictureBox imgLebron;
+        private System.Windows.Forms.RadioButton rbtnDecimal;
+        private System.Windows.Forms.RadioButton rbtnHex;
+        private System.Windows.Forms.RadioButton rbtnOctal;
+        private System.Windows.Forms.RadioButton rbtnBin;
     }
 }
 
--- Desarrollo de Interfaces/009_Conversor-numerico/Form1.cs
+++ Desarrollo de Interfaces/009_Conversor-numerico/Form1.cs
@@ -27,15 +27,63 @@ namespace _009_Conversor_numerico
             this.Close();
         }
 
+        private string formatBinary(string rawBinary)
+        {
+            string binSpaced = "";
+            int charCount = 1;
+            for (int i = 1; i < rawBinary.Length; i++, charCount++)
+            {
+                binSpaced += rawBinary.Substring(i, 1);
+                if (charCount > 4)
+                {
+                    binSpaced += " ";
+                    charCount = 0;
+                }
+            }
+            return binSpaced;
+        }
+
+
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            int value = Int32.Parse(txtDecimal.Text);
+            if (rbtnDecimal.Checked)
+            {
+                int value = Int32.Parse(txtDecimal.Text);
 
-            string bin = Convert.ToString(value, 2);
+                string bin = Convert.ToString(value, 2);
+                txtBinary.Text = formatBinary(bin);
+                txtOctal.Text = Convert.ToString(value, 8);
+                string hex = Convert.ToString(value, 16);
+                txtHex.Text = hex.ToUpper();
+            }
+            if (rbtnBin.Checked)
+            {
+                Int64 value = Convert.ToInt64(txtBinary.Text, 2);
 
-            txtOctal.Text = Convert.ToString(value, 8);
-            string hex = Convert.ToString(value, 16);
-            txtHex.Text = hex.ToUpper();
+                txtDecimal.Text = Convert.ToString(value, 10);
+                txtOctal.Text = Convert.ToString(value, 8);
+                string hex = Convert.ToString(value, 16);
+                txtHex.Text = hex.ToUpper();
+            }
+            if (rbtnOctal.Checked)
+            {
+                Int64 value = Convert.ToInt64(txtOctal.Text, 8);
+
+                txtDecimal.Text = Convert.ToString(value, 10);
+                string bin = Convert.ToString(value, 2);
+                txtBinary.Text = formatBinary(bin);
+                string hex = Convert.ToString(value, 16);
+                txtHex.Text = hex.ToUpper();
+            }
+            if (rbtnHex.Checked)
+            {
+                Int64 value = Convert.ToInt64(txtHex.Text, 16);
+
+                txtDecimal.Text = Convert.ToString(value, 10);
+                string bin = Convert.ToString(value, 2);
+                txtBinary.Text = formatBinary(bin);
+                txtOctal.Text = Convert.ToString(value, 8);
+            }
         }
 
         private string splitEachFourChar(string str)
@@ -48,6 +96,38 @@ namespace _009_Conversor_numerico
             }
 
             return "";
+        }
+
+        private void rbtnDecimal_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDecimal.Enabled = true;
+            txtBinary.Enabled = false;
+            txtOctal.Enabled = false;
+            txtHex.Enabled = false;
+        }
+
+        private void rbtnBin_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDecimal.Enabled = false;
+            txtBinary.Enabled = true;
+            txtOctal.Enabled = false;
+            txtHex.Enabled = false;
+        }
+
+        private void rbtnOctal_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDecimal.Enabled = false;
+            txtBinary.Enabled = false;
+            txtOctal.Enabled = true;
+            txtHex.Enabled = false;
+        }
+
+        private void rbtnHex_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDecimal.Enabled = false;
+            txtBinary.Enabled = false;
+            txtOctal.Enabled = false;
+            txtHex.Enabled = true;
         }
     }
 }
