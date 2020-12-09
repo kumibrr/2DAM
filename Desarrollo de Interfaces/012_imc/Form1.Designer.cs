17d153b348a8085a466505cd838def80a85b1a77
kumibrr <isra.lora99@gmail.com>
2020-12-09 21:43:31 +0100

    DIejercicioIMC

--- Desarrollo de Interfaces/012_imc/Form1.Designer.cs
+++ Desarrollo de Interfaces/012_imc/Form1.Designer.cs
@@ -31,12 +31,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.sbWeight = new System.Windows.Forms.VScrollBar();
+            this.tbHeight = new System.Windows.Forms.TrackBar();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -44,8 +46,9 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(627, 50);
+            this.label1.Size = new System.Drawing.Size(940, 77);
             this.label1.TabIndex = 0;
             this.label1.Text = "Índice de Masa Corporal IMC";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -54,9 +57,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 100);
+            this.label2.Location = new System.Drawing.Point(78, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(128, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Altura (cm)";
             // 
@@ -64,65 +68,91 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(187, 100);
+            this.label3.Location = new System.Drawing.Point(280, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.Size = new System.Drawing.Size(121, 29);
             this.label3.TabIndex = 2;
             this.label3.Text = "Peso (Kg)";
             // 
-            // vScrollBar1
+            // sbWeight
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(214, 139);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 239);
-            this.vScrollBar1.TabIndex = 3;
+            this.sbWeight.Location = new System.Drawing.Point(327, 214);
+            this.sbWeight.Maximum = 200;
+            this.sbWeight.Minimum = 1;
+            this.sbWeight.Name = "sbWeight";
+            this.sbWeight.Size = new System.Drawing.Size(17, 368);
+            this.sbWeight.TabIndex = 3;
+            this.sbWeight.Value = 200;
+            this.sbWeight.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sbWeight_Scroll);
             // 
-            // trackBar1
+            // tbHeight
             // 
-            this.trackBar1.Location = new System.Drawing.Point(84, 139);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.Size = new System.Drawing.Size(45, 239);
-            this.trackBar1.TabIndex = 4;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.tbHeight.Location = new System.Drawing.Point(126, 214);
+            this.tbHeight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbHeight.Maximum = 300;
+            this.tbHeight.Minimum = 5;
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tbHeight.Size = new System.Drawing.Size(69, 368);
+            this.tbHeight.TabIndex = 4;
+            this.tbHeight.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.tbHeight.Value = 5;
+            this.tbHeight.Scroll += new System.EventHandler(this.tbHeight_Scroll);
             // 
-            // textBox1
+            // txtHeight
             // 
-            this.textBox1.Location = new System.Drawing.Point(84, 384);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(45, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtHeight.Location = new System.Drawing.Point(126, 591);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(66, 26);
+            this.txtHeight.TabIndex = 5;
             // 
-            // textBox2
+            // txtWeight
             // 
-            this.textBox2.Location = new System.Drawing.Point(201, 384);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(45, 20);
-            this.textBox2.TabIndex = 6;
+            this.txtWeight.Location = new System.Drawing.Point(302, 591);
+            this.txtWeight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(66, 26);
+            this.txtWeight.TabIndex = 6;
             // 
-            // textBox3
+            // txtIMC
             // 
-            this.textBox3.Location = new System.Drawing.Point(452, 324);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.txtIMC.Location = new System.Drawing.Point(678, 498);
+            this.txtIMC.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(148, 26);
+            this.txtIMC.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_012_imc.Properties.Resources.PesoBajo;
+            this.pictureBox1.Location = new System.Drawing.Point(678, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 307);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.vScrollBar1);
+            this.ClientSize = new System.Drawing.Size(940, 692);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.sbWeight);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,11 +163,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.VScrollBar sbWeight;
+        private System.Windows.Forms.TrackBar tbHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Desarrollo de Interfaces/012_imc/Form1.cs
+++ Desarrollo de Interfaces/012_imc/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,60 @@ namespace _012_imc
 {
     public partial class Form1 : Form
     {
+        private double height = 1;
+        private double weight = 1;
+        private double imc = 0;
+        static string projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        static string filePath = Path.Combine(projectPath, "Resources");
+
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml("#e5dece");
+        }
+
+        private void updateIMC()
+        {
+            if(height != 0)
+            {
+                imc = weight / (height / 100 * 2);
+            }
+
+            txtIMC.Text = imc.ToString();
+
+            if (imc < 18.5)
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\PesoBajo.jpg");
+            } else if (imc < 25)
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\PesoIdeal.jpg");
+            } else if (imc < 30)
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\Sobrepeso.jpg");
+            } else if (imc < 35)
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\Obeso.jpg");
+            } else if (imc < 40)
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\ObesidadSevera.jpg");
+            } else
+            {
+                pictureBox1.Image = Image.FromFile(filePath + "\\ObesidadMorbida.jpg");
+            }
+        }
+
+        private void tbHeight_Scroll(object sender, EventArgs e)
+        {
+            height = tbHeight.Value;
+            txtHeight.Text = tbHeight.Value.ToString();
+            updateIMC();
+        }
+
+        private void sbWeight_Scroll(object sender, ScrollEventArgs e)
+        {
+            weight = sbWeight.Maximum - sbWeight.Value;
+            txtWeight.Text = weight.ToString();
+            updateIMC();
         }
     }
 }
