49b65ec9e1ceb4c4f5e819ccc3d5780fa8aa981a
kumibrr <isra.lora99@gmail.com>
2021-01-13 12:43:31 +0100

    Ejercicio XML en C#

---
+++ Desarrollo de Interfaces/019_LecturaXML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _019_LecturaXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
