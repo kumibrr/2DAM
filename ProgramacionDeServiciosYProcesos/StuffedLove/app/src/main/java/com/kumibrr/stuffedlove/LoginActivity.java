19f0e01972605baf203bc294a66b5d9ac51fa4cd
kumibrr <isra.lora99@gmail.com>
2021-01-18 19:06:50 +0100

    Ejercicio Tienda

---
+++ ProgramacionDeServiciosYProcesos/StuffedLove/app/src/main/java/com/kumibrr/stuffedlove/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.kumibrr.stuffedlove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends CoreActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        super.setLayout(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
+
+    }
+
+
+}
