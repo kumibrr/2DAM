1c032f6e8f53a5f1d3bdc2b1887f6fb3493f5fa8
kumibrr <isra.lora99@gmail.com>
2021-01-18 20:21:37 +0100

    Ejercicio PSP: Hechas varios activities.

--- ProgramacionDeServiciosYProcesos/StuffedLove/app/src/main/java/com/kumibrr/stuffedlove/MainActivity.java
+++ ProgramacionDeServiciosYProcesos/StuffedLove/app/src/main/java/com/kumibrr/stuffedlove/MainActivity.java
@@ -16,16 +16,5 @@ public class MainActivity extends CoreActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
---
+++ ProgramacionDeServiciosYProcesos/StuffedLove/app/src/main/java/com/kumibrr/stuffedlove/OrderActivity.java
@@ -0,0 +1,14 @@
+package com.kumibrr.stuffedlove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
