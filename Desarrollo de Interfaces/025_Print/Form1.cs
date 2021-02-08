e77833d79684f592cc82083f03e232a4c25c8e62
kumibrr <isra.lora99@gmail.com>
2021-02-08 14:35:43 +0100

    Clase DI.

---
+++ Desarrollo de Interfaces/025_Print/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _025_Print
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrintDocument p = new PrintDocument();
+            Font miFuente = new Font("Arial", 16, FontStyle.Bold);
+
+            printDialog1.Document = p;
+            printDialog1.ShowDialog();
+        }
+    }
+}
