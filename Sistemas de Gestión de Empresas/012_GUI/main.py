9a59f5da2c226c38977e2699207f8384063165eb
kumibrr <isra.lora99@gmail.com>
2021-02-09 19:43:25 +0100

    Ejercicio SGE.

--- Sistemas de Gestión de Empresas/012_GUI/main.py
+++ Sistemas de Gestión de Empresas/012_GUI/main.py
@@ -1,11 +1,15 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("400x300+50+50")
+window.geometry("400x300+0+0")
 window2 = Tk()
 window2.geometry("400x300-0-0")
+window3 = Tk()
+window3.geometry("400x300-0+0")
+window4 = Tk()
+window4.geometry("400x300+0-0")
 
-print(window.maxsize())
-
-#window.mainloop()
-#window2.mainloop()
+window.mainloop()
+window2.mainloop()
+window3.mainloop()
+window4.mainloop()
