7ebeac394fa59695fd4786c5c9d967efb4d6c872
kumibrr <isra.lora99@gmail.com>
2021-02-14 01:23:11 +0100

    (Incidences) fixed checkboxes.

--- Programación Multimedia y Dispositivos Móviles/009/Incidencias/src/app/pages/add-incidence/add-incidence.page.ts
+++ Programación Multimedia y Dispositivos Móviles/009/Incidencias/src/app/pages/add-incidence/add-incidence.page.ts
@@ -8,34 +8,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddIncidencePage implements OnInit {
 
-  public level = [
-    {value: 0, class: 'low', note: 'Leve', isChecked: false},
-    {value: 1, class: 'medium', note: 'Medio', isChecked: false},
-    {value: 2, class: 'high', note: 'Alto', isChecked: false},
-    {value: 3, class: 'extreme', note: 'Extremo', isChecked: false}
-  ];
   public form: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.form = formBuilder.group({
       title: ['', Validators.required],
       description: [''],
-      level: ['', Validators.required]
+      low: [false],
+      medium: [false],
+      high: [false],
+      extreme: [false]
     });
   }
 
   ngOnInit() {
   }
 
-  checkChange(checked): void {
-    console.log(checked);
-    this.level.forEach(c => {
-      if (c.value !== checked.value) {
-        c.isChecked = false;
-      } else {
-        c.isChecked = true;
-      }
-    });
+  checkChange(c: number): void {
+    console.log(this.form);
+    this.form.controls.low.setValue(false);
+    this.form.controls.medium.setValue(false);
+    this.form.controls.high.setValue(false);
+    this.form.controls.extreme.setValue(false);
   }
 
 }
